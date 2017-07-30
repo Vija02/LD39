4e0fbc3913a539d7049b447a4cdc7d8d21ed8267
Vija02 <salim_michaelmi@yahoo.com>
2017-07-30 12:00:55 +0700

    some level design woooo

--- src/containers/Game/1/Blocks.js
+++ src/containers/Game/1/Blocks.js
@@ -3,10 +3,12 @@ import Background from 'components/Background'
 import Bandage from 'components/Bandage'
 
 import moss from 'assets/moss.png'
+import bloody_moss from 'assets/bloody_moss.png'
 import bandages from 'assets/bandages.png'
 
 export default {
   1: <Background color="black" solid />,
   2: <Background src={[moss]} />,
-  3: <Bandage src={[moss, bandages]} solid />,
+  3: <Background src={[bloody_moss]} />,
+  4: <Bandage src={[moss, bandages]} solid />,
 }
