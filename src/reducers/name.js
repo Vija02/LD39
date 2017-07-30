1d598bb77004cff799ed7aeb88a07670b4b934be
Vija02 <salim_michaelmi@yahoo.com>
2017-07-31 04:54:53 +0700

    Build

--- src/reducers/name.js
+++
@@ -1,8 +0,0 @@
-module.exports = (state = {name: null}, action) => {
-  switch (action.type) {
-    case 'SET_NAME':
-      return {...state, name: action.name};
-    default:
-      return state;
-  }
-};
